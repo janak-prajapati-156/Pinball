acd6fae8a4aca0d521dbfaf0a6c96e89f37a9444
janak-prajapati-156 <janakprajapati156@gmail.com>
2021-04-20 13:06:38 -0400

    tp0 v1

--- pinball.py
+++ pinball.py
@@ -1,3 +1,14 @@
 import numpy as np, cv2
 from cmu_112_graphics import *
-#code for the pinball
+
+class pinball(object):
+    def __init__(self):
+        self.radius = "janak"
+
+    def appStarted(app, self):
+        app.bool = False
+
+    def redrawAll(app, canvas, self):
+        canvas.create_text(app.width/2, app.height/2, text = str(self.radius))
+
+    runApp(width=650, height=550)
--- testCV2.py
+++ testCV2.py
@@ -25,34 +25,34 @@ def redrawAll(app, canvas):
 def singularBlue(app):
     app.isBlue = False
     testVid = cv2.VideoCapture(0)
-    testVid.set(3, 640)
-    testVid.set(4, 480)
-    testVid.set(10, 100)
-    test, image = testVid.read()
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lowBlue = np.array([110,50,50])
-    upBlue = np.array([130,255,255])
-    mask = cv2.inRange(hsv, lowBlue, upBlue)
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.dilate(mask, kernel)
-    res = cv2.bitwise_and(image, image, mask=mask)
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, 
-                                            cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if area>7000:
-            app.isBlue = True
-    testVid.release()
+    if testVid.isOpened():
+        test, image = testVid.read()
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lowBlue = np.array([110,50,50])
+        upBlue = np.array([130,255,255])
+        mask = cv2.inRange(hsv, lowBlue, upBlue)
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.dilate(mask, kernel)
+        res = cv2.bitwise_and(image, image, mask=mask)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, 
+                                                cv2.CHAIN_APPROX_SIMPLE)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if area>7000:
+                app.isBlue = True
+        testVid.release()
     cv2.destroyAllWindows()
     
-def blueDetection(app):
-    app.isBlue = False
+def blueDetection():
+    check = False
     testVid = cv2.VideoCapture(0)
-    testVid.set(3, 640)
-    testVid.set(4, 480)
-    testVid.set(10, 100)
+    # testVid.set(3, 640)
+    # testVid.set(4, 480)
+    # testVid.set(10, 100)
     while True:
         test, image = testVid.read()
+        areaList = []
+        # print(testVid.isOpened())
         # image = cv2.Canny(image, 100, 100)
         # image = cv2.dilate(image, kernel, iterations = 1)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -67,22 +67,20 @@ def blueDetection(app):
                                             cv2.CHAIN_APPROX_SIMPLE)
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
+            areaList.append(area)
             if area>7000:
-                app.isBlue = True
-                cv2.waitKey(1)
-                cv2.destroyAllWindows()
-                for i in range (1,5):
-                    cv2.waitKey(1)
-                break
-                # x, y, w, h = cv2.boundingRect(contour)
-                # image = cv2.rectangle(image, (x, y), 
-                #                         (x + w, y + h), 
-                #                         (0, 0, 255), 2)
-                # cv2.putText(image, "Blue Colour", (x, y),
-                #             cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                #             (0, 0, 255))
-        # if(app.isBlue):
-        break
+                x, y, w, h = cv2.boundingRect(contour)
+                image = cv2.rectangle(image, (x, y), 
+                                        (x + w, y + h), 
+                                        (0, 0, 255), 2)
+                cv2.putText(image, "Blue Colour", (x, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                            (0, 0, 255))
+
+        if areaList!=[] and max(areaList)>7000:
+            print(True)
+        else:
+            print(False)
         # b = res[:, :, :1]
         # g = res[:, :, 1:2]
         # r = res[:, :, 2:]
@@ -97,17 +95,17 @@ def blueDetection(app):
         #     print("Red")
 
         cv2.imshow("output", image)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-            # cv2.waitKey(1)
-            # cv2.destroyAllWindows()
-            # for i in range (1,5):
-            #     cv2.waitKey(1)
-            # break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.waitKey(1)
+            cv2.destroyAllWindows()
+            for i in range (1,5):
+                cv2.waitKey(1)
+            break
 
     testVid.release()
     cv2.destroyAllWindows()
 
-
-runApp(width=400, height=400)
+blueDetection()
+# runApp(width=400, height=400)
 
 
